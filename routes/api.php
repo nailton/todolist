1cf18fd6eee36663605a57387e23e5310a7679cd
Nailton Sousa <nailton.osn@gmail.com>
2021-01-01 19:09:02 -0300

    Vue components and REST

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/items', [\App\Http\Controllers\ItemController::class, 'index']);
+Route::prefix('/item')->group( function () {
+    Route::post('/store', [\App\Http\Controllers\ItemController::class, 'store']);
+    Route::put('/{id}', [\App\Http\Controllers\ItemController::class, 'update']);
+    Route::delete('/{id}', [\App\Http\Controllers\ItemController::class, 'destroy']);
+});
